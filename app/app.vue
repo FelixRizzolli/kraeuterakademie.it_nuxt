729dc3789489d4ca248c200b6b2b6517f90af495
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-04-30 07:13:12 +0000

    feat(HeroSmall): add HeroSmall component

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <ContentHeroSmall />
   </div>
 </template>
 
---
+++ app/components/Content/HeroSmall.vue
@@ -0,0 +1,89 @@
+<template>
+  <section class="contentelement_herosmall grid-container">
+    <div class="content">
+      <h1 class="title">{{ title }}</h1>
+      <NuxtLink class="link-button" :to="link?.href">{{ link?.text }}</NuxtLink>
+    </div>
+    <div class="image">
+      <img
+        :src="image?.src"
+        :alt="image?.alt"
+        class="hero-image" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { ref, onBeforeMount } from 'vue';
+
+interface Link {
+  href: string;
+  text: string;
+}
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+const title = ref<string>('Kurse zu Wildpflanzen und Heilkräuter');
+const link = ref<Link>({
+  href: '/akademie',
+  text: 'Kräuterakademie',
+});
+const image = ref<Image>({
+  src: '/images/herosmall_1.png',
+  alt: 'Kräuterakademie',
+});
+
+onBeforeMount(() => {
+  console.log('HeroSmall component mounted');
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+.contentelement_herosmall {
+  padding-top: 30rem;
+  position: relative;
+}
+
+.content {
+  @include col-start(3);
+  @include col(4);
+
+  & {
+    z-index: 1;
+    grid-row: 1;
+
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.title {
+  @include fontSizeH1();
+
+  & {
+    text-align: right;
+  }
+}
+
+.link-button {
+  margin-top: 2.5rem;
+  align-self: flex-end;
+}
+
+.image {
+  @include col-start(6);
+  @include col(6);
+
+  & {
+    margin-top: 17rem;
+    grid-row: 1;
+    z-index: 0;
+  }
+}
+
+</style>
