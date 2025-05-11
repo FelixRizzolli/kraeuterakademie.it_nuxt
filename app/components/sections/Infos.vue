3fb2b5dbe0db775fcd3bd75eb157a4bc2996dc98
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-11 06:35:22 +0000

    feat(Infos): add Infos component

---
+++ app/components/sections/Infos.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="contentelement_template grid-container">
+        <div class="info" v-for="(info, index) in infos" :key="index">
+            <h3 class="title">{{ info.title }}</h3>
+            <p class="text">{{ info.text }}</p>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+export interface TemplateProps {
+    infos: Array<{
+        title: string;
+        text: string;
+    }>;
+}
+
+const props = defineProps<TemplateProps>();
+</script>
+
+<style lang="scss" scoped>
+.contentelement_template {
+    padding: 15rem 0;
+
+    background-color: $colorBackgroundBrown;
+    color: $colorFontWhite;
+}
+
+.info {
+    @include col(4);
+
+    &:nth-child(3n + 1) {
+        @include col-start(2);
+    }
+
+    &:nth-child(3n + 2) {
+        @include col-start(6);
+        margin-top: 13.5rem;
+    }
+
+    &:nth-child(3n) {
+        @include col-start(10);
+        margin-top: 6rem;
+    }
+}
+
+.title {
+    @include font-24-39-5-L();
+    @include fontFamilyDefault();
+
+    & {
+        font-weight: 700;
+    }
+}
+
+.text {
+    @include font-24-39-5-L();
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,8 @@
         <HeroSmall :title="pageData.herosmall.title" :image="pageData.herosmall.image" :link="pageData.herosmall.link" />
         <ImageText :image="pageData.imagetext.image" :text1="pageData.imagetext.text1" :infos="pageData.imagetext.infos" :text2="pageData.imagetext.text2" :link="pageData.imagetext.link" />
         <AnimatedText :text="pageData.animatedtext.text" />
+        <MoodPicture :image="pageData.moodpicture.image" />
+        <Infos :infos="pageData.infos" />
     </div>
 </template>
 
@@ -35,6 +37,26 @@ const pageData = ref<any>({
     animatedtext: {
         text: "Ich bin ein kleiner Blindtext. Und zwar schon so lange ich denken kann. Es war nicht leicht zu verstehen, was es bedeutet, ein blinder Text zu sein: Man ergibt keinen Sinn. Wirklich keinen Sinn. Man wird zusammenhangslos eingeschoben und rumgedreht - und oftmals gar nicht erst gelesen. Aber bin ich allein deshalb ein Text.",
     },
+    moodpicture: {
+        image: {
+            src: "/images/herobig_1.png",
+            alt: "Kräuterakademie",
+        },
+    },
+    infos: [
+        {
+            title: "Lehrgangskosten € 270 + Mwst",
+            text: "Im Preis inbegriffen sind umfangreiche Unterlagen und Bestimmungshilfen",
+        },
+        {
+            title: "Teilnehmerzahl ist begrenzt",
+            text: "Bei Fragen oder Infos: 338 269 8477 oder sigrid.thaler@gmail.com",
+        },
+        {
+            title: "Referentin Sigrid Thaler",
+            text: "Referentin der FNL Kräuterakademie",
+        },
+    ],
 });
 </script>
 
