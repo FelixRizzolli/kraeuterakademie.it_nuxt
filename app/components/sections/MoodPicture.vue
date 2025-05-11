fdeac8e780d7e8925ec1f1036ee3c821c8e436d9
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-11 06:17:13 +0000

    feat(MoodPicture): add MoodPicture component

---
+++ app/components/sections/MoodPicture.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="contentelement_moodpicture">
+        <div class="image-container">
+            <img :src="image?.src" :alt="image?.alt" class="image" />
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+interface Image {
+    src: string;
+    alt: string;
+}
+
+export interface MoodPictureProps {
+    image: Image;
+}
+
+const props = defineProps<MoodPictureProps>();
+</script>
+
+<style lang="scss" scoped>
+.image-container {
+    margin-left: 21rem;
+}
+
+.image {
+    width: 100%;
+    height: auto;
+}
+</style>
