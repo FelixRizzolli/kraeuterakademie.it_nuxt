b063f9cba5357c0923462fcd24a0630839672c41
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-11 06:35:55 +0000

    chore(Template): add Template component

---
+++ app/components/sections/Template.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="contentelement_template grid-container"></section>
+</template>
+
+<script lang="ts" setup>
+export interface TemplateProps {}
+
+const props = defineProps<TemplateProps>();
+</script>
+
+<style lang="scss" scoped></style>
