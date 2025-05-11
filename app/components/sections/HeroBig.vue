542589641c30ebdad074723d131f15672788999a
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-11 07:59:23 +0000

    feat(HeroBig): add HeroBig component

---
+++ app/components/sections/HeroBig.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="contentelement_herobig">
+        <div class="image-container">
+            <img :src="image?.src" :alt="image?.alt" class="image" />
+        </div>
+        <div class="content-container grid-container">
+            <h1 class="title">{{ title }}</h1>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+interface Image {
+    src: string;
+    alt: string;
+}
+
+export interface HeroBigProps {
+    title: string;
+    image: Image;
+}
+
+const props = defineProps<HeroBigProps>();
+</script>
+
+<style lang="scss" scoped>
+.contentelement_herobig {
+    padding-top: 30rem;
+}
+
+.title {
+    @include col-start(3);
+    @include col(6);
+
+    & {
+        z-index: 1;
+
+        text-align: right;
+    }
+}
+
+.image-container {
+    position: relative;
+    margin-left: 21rem;
+}
+
+.image {
+    margin-top: 3.5rem;
+    position: absolute;
+    width: 100%;
+    height: auto;
+}
+</style>
--- app/pages/kraeuterkurse/index.vue
+++ app/pages/kraeuterkurse/index.vue
@@ -1,5 +1,17 @@
-<template></template>
+<template>
+    <HeroBig :title="pageData.herobig.title" :image="pageData.herobig.image" />
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const pageData = ref<any>({
+    herobig: {
+        title: "Kräuterkurs Titel",
+        image: {
+            src: "/images/herobig_1.png",
+            alt: "Kräuterkurs Bild",
+        },
+    },
+});
+</script>
 
 <style lang="scss" scoped></style>
