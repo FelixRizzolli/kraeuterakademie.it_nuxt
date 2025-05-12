5fe6d655d5a5a09bc5e5042d17dbc68f87cbcd62
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-12 13:23:57 +0000

    data: add content for page kraueterkurse

--- app/pages/kraeuterkurse/index.vue
+++ app/pages/kraeuterkurse/index.vue
@@ -1,59 +1,141 @@
 <template>
     <HeroBig :title="pageData.herobig.title" :image="pageData.herobig.image" />
     <Accordions :title="pageData.accordions.title" :accordions="pageData.accordions.accordions" />
-    <TextElement :title="pageData.textelement.title" :text="pageData.textelement.content" :link="pageData.textelement.link" />
+    <CourseList :title="pageData.nextdates.title" :courses="pageData.nextdates.courses" />
+    <Infos :infos="pageData.infos" />
     <CourseList :title="pageData.courselist.title" :courses="pageData.courselist.courses" />
 </template>
 
 <script lang="ts" setup>
 const pageData = ref<any>({
     herobig: {
-        title: "Kräuterkurs Titel",
+        title: "Volksheilkundlicher Kräuterkurs nach Ignaz Schlifni",
         image: {
             src: "/images/herobig_1.png",
             alt: "Kräuterkurs Bild",
         },
     },
     accordions: {
-        title: "Titel zum Programm",
+        title: "Ganzheitliche Kräuterausbildung in Südtirol mit Abschluss",
         accordions: [
             {
-                title: "Botanische Grundkenntnisse & Pflanzeninhaltsstoffe",
-                content: "Asklepius, Gott der Heilkunst, verkündete schon vor 3000 Jahren: „Zuerst das Wort, dann die Heilpflanze und zuletzt das Messer“ In der Volksheilkunde suchen wir nicht nach Antimittel, sondern nach den Kräften der Natur, welche die Selbstheilungskräfte anregen und wieder einen Ausgleich schaffen. Dies bewirkt nicht nur der stoffliche Bereich, das immaterielle Wirkprinzip darf nicht unterschätzt werden.",
+                title: "Kennenlernen von über 400 Pflanzen",
+                content: String.raw`
+                    <p>
+                        Tauche ein in die faszinierende Pflanzenwelt und lerne über <strong>400 heimische Pflanzen</strong> kennen, darunter Bäume, Sträucher, Kräuter und Giftpflanzen. Du wirst ihre Erkennungsmerkmale, Verwandtschaften und Anwendungen entdecken.
+                    </p>
+                `,
             },
             {
-                title: "Traditioneller Gebrauch in der Volksheilkunde",
-                content: "Asklepius, Gott der Heilkunst, verkündete schon vor 3000 Jahren: „Zuerst das Wort, dann die Heilpflanze und zuletzt das Messer“ In der Volksheilkunde suchen wir nicht nach Antimittel, sondern nach den Kräften der Natur, welche die Selbstheilungskräfte anregen und wieder einen Ausgleich schaffen. Dies bewirkt nicht nur der stoffliche Bereich, das immaterielle Wirkprinzip darf nicht unterschätzt werden.",
+                title: "Botanische Grundkenntnisse",
+                content: String.raw`
+                    <p>
+                        Mit einem soliden <strong>botanischen Grundwissen</strong> kannst du Pflanzen präzise betrachten, einteilen und selbstständig bestimmen. So erhälst du einen ganzheitlichen <strong>Zugang zur Natur</strong>.
+                    </p>
+                `,
             },
             {
-                title: "Anatomie, Homöopathie, Bachblüten und Spagyrik",
-                content: "Asklepius, Gott der Heilkunst, verkündete schon vor 3000 Jahren: „Zuerst das Wort, dann die Heilpflanze und zuletzt das Messer“ In der Volksheilkunde suchen wir nicht nach Antimittel, sondern nach den Kräften der Natur, welche die Selbstheilungskräfte anregen und wieder einen Ausgleich schaffen. Dies bewirkt nicht nur der stoffliche Bereich, das immaterielle Wirkprinzip darf nicht unterschätzt werden.",
+                title: "Pflanzeninhaltsstoffe & Volksheilkunde",
+                content: String.raw`
+                    <p>
+                        Um die Inhaltsstoffe von Pflanzen effektiv in der <strong>Volksheilkunde</strong> zu nutzen, ist es wichtig, deren Eigenschaften zu verstehen. Du erfährst, wie <strong>Erntezeitpunkt, Konservierung und Extraktion die Wirksamkeit</strong> beeinflussen. Der Fokus liegt auf den natürlichen Kräften, die unsere Selbstheilung fördern und Körper, Geist und Seele ins Geleichgewicht bringen.
+                    </p>
+                `,
             },
             {
-                title: "Ätherische Öle, Hydrolate, Destillation",
-                content: "Asklepius, Gott der Heilkunst, verkündete schon vor 3000 Jahren: „Zuerst das Wort, dann die Heilpflanze und zuletzt das Messer“ In der Volksheilkunde suchen wir nicht nach Antimittel, sondern nach den Kräften der Natur, welche die Selbstheilungskräfte anregen und wieder einen Ausgleich schaffen. Dies bewirkt nicht nur der stoffliche Bereich, das immaterielle Wirkprinzip darf nicht unterschätzt werden.",
+                title: "Anatomie des menschlichen Körpers",
+                content: String.raw`
+                    <p>
+                        Ein weiterer wichtiger Aspekt ist das Verständnis deines "inneren Arztes". Durch das Studium der <strong>Anatomie</strong> deines Körpers unterstützt du deine Selbstheilungskräfte.
+                    </p>
+                `,
             },
             {
-                title: "Räuchern, Brauchtum, Jahreskreisfeste",
-                content: "Asklepius, Gott der Heilkunst, verkündete schon vor 3000 Jahren: „Zuerst das Wort, dann die Heilpflanze und zuletzt das Messer“ In der Volksheilkunde suchen wir nicht nach Antimittel, sondern nach den Kräften der Natur, welche die Selbstheilungskräfte anregen und wieder einen Ausgleich schaffen. Dies bewirkt nicht nur der stoffliche Bereich, das immaterielle Wirkprinzip darf nicht unterschätzt werden.",
+                title: "Ganzheitliche Ansätze",
+                content: String.raw`
+                    <p>
+                        Es werden ganzheitliche Ansätze wie <strong>Homöopathie, Bachblüten, Spagyrik und Gemmotherapie</strong> behandelt. Lerne auch über ätherische Öle und Hydrolate sowie deren Anwendung für dein Wohlbefinden.
+                    </p>
+                `,
+            },
+            {
+                title: "Altes Wissen, Brauchtum",
+                content: String.raw`
+                    <p>
+                        Erwecke fast vergessenes Wissen zum Leben! <strong>Mythologie, Brauchtum und Jahreskreisfeste</strong> bieten wertvolle Einblicke in alte Traditionen. Das Räuchern verbindet dich auf wunderbare Weise mit dem Wesen der Pflanzen.
+                    </p>
+                `,
+            },
+            {
+                title: "Dauer der Ausbildung",
+                content: String.raw`
+                    <p>
+                        Die Ausbildung dauert eineinhalb bis zwei Jahre und umfasst mindestens 200 Unterrichtseinheiten, verteilt auf 30 Kurstage, einer Lehrfahrt sowie Prüfungsvorbereitungseinheiten. Gastreferenten bereichern den Kurs mit ihrem Wissen.
+                    </p>
+                    <p>
+                        <strong>Die Kurszeigten sind von 9:00 bis 19:00 Uhr.</strong>
+                    </p>
+                    <p>
+                        Die <strong>Diplomprüfung</strong> findet im <strong>Schloss Hunnenbrunn</strong> statt; alternativ erhält man einen Kursnachweis.
+                    </p>
+                    <p>
+                        Die Kurse starten jährlich in verschiedenen Landesteilen in Südtirol.
+                    </p>
+                `,
+            },
+            {
+                title: "Berufliche Möglichkeiten",
+                content: String.raw`
+                    <p>
+                        Die Ausbildung bietet zahlreiche berufliche Perspektiven, darunter das Anbieten von <strong>Kräuterführungen, Seminaren und Workshops</strong> für Hotels, Tourismusverbände und Schulen. Mit dem Diplom kannst du dem <strong>Verein der Südtiroler Kräuterpädagogen</strong> beitreten.
+                    </p>
+                `,
             },
         ],
     },
-    textelement: {
-        title: "Titel zu Zertifikat und Datum",
-        link: {
-            text: "Hier geht's zur Anmeldung",
-            href: "/anmeldung",
-        },
-        content: String.raw`
-            <p>Das Zertifikat zur/m Knospenexpert*in erhält man nach der Prüfung, bei der man das Gelernte präsentieren kann.</p>
-            <p>Der dreitägige Lehrgang wird in Südtirol 2025 zweimal angeboten, und die Uhrzeiten sind jeweils von 8.30 bis 17.30 Uhr:</p>
-            <ul>
-                <li>Freitag: 28. Februar, 07. März, 21. März bei Bergila</li>
-                <li>Samstag: 01. März, 08. März, 22. März in Montan</li>
-            </ul>
-        `,
+    nextdates: {
+        title: "Kommende Kurse",
+        courses: [
+            {
+                title: "Unterland K25",
+                place: "Unterland",
+                fromDate: "2025-10-04",
+                toDate: "2027-08-07",
+                description: "Der Kurs läuft in Montan, im Unterland von Oktober 2025 bis August 2027. Die Kurstage sind immer am Samstag und finden in der Zeit von 9:00 bis 17:00 Uhr statt.",
+                link: {
+                    text: "Kräuterkurs Anmelden",
+                    href: "/kurse/unterland-k25",
+                },
+                dates: ["2025-10-04", "2025-11-08", "2025-11-22", "2026-01-10", "2026-01-31", "2026-02-14", "2026-03-21", "2026-04-04", "2026-05-16", "2026-05-30", "2026-06-13", "2026-07-11", "2026-08-01", "2026-08-22", "2026-09-12", "2026-09-26", "2026-10-10", "2026-11-14", "2026-12-05", "2027-01-16", "2027-02-13", "2027-02-27", "2027-03-13", "2027-04-03", "2027-04-24", "2027-05-08", "2027-05-29", "2027-06-19", "2027-07-03", "2027-07-17", "2027-08-07"],
+            },
+            {
+                title: "Piustertal K26",
+                place: "Pustertal",
+                fromDate: "2025-10-09",
+                toDate: "2027-08-05",
+                description: "Der Kurs läuft im Pustertal von Oktober 2025 bis August 2027. Die Kurstage sind immer am Donnerstag und finden in der Zeit von 9:00 bis 17:00 Uhr statt.",
+                link: {
+                    text: "Kräuterkurs Anmelden",
+                    href: "/kurse/pustertal-k26",
+                },
+                dates: ["2025-10-09", "2025-11-06", "2025-11-27", "2026-01-15", "2026-01-29", "2026-02-19", "2026-03-19", "2026-04-09", "2026-05-09", "2026-05-14", "2026-06-18", "2026-07-09", "2026-07-30", "2026-08-20", "2026-09-17", "2026-10-01", "2026-10-15", "2026-11-12", "2026-12-03", "2027-01-14", "2027-02-11", "2027-03-04", "2027-03-18", "2027-04-08", "2027-04-18", "2027-05-08", "2027-05-29", "2027-06-03", "2027-06-24", "2027-07-08", "2027-07-22", "2027-08-05"],
+            },
+        ],
     },
+    infos: [
+        {
+            title: "Kosten € 2080 + Mwst",
+            text: "Im Preis inbegriffen sind umfangreiche Unterlagen, Verwaltungskosten, der FNL Mitgliedsbeitrag und die Prüfungsgebühr.",
+        },
+        {
+            title: "Teilnehmerzahl ist begrenzt",
+            text: "Bei Fragen oder Infos: 338 269 8477 oder sigrid.thaler@gmail.com",
+        },
+        {
+            title: "Referentin Sigrid Thaler",
+            text: "Referentin der FNL Kräuterakademie",
+        },
+    ],
     courselist: {
         title: "Aktuelle Kurse",
         courses: [
@@ -67,7 +149,6 @@ const pageData = ref<any>({
                     text: "Kräuterkurs Anmelden",
                     href: "/kurse/pustertal-k20",
                 },
-                dates: ["28.10.2023 SA", "11.11.2023 SA", "09.12.2023 SO", "20.01.2024 SA", "10.02.2024 SO", "23.03.2024 SA", "27.04.2024 SA", "11.05.2024 SA", "08.06.2024 SA", "06.07.2024 SO", "03.08.2024 SO", "31.08.2024 MI", "14.09.2024 SA", "28.09.2024 SO", "12.10.2024 SA", "09.11.2024 SA", "23.11.2024 SA", "07.12.2024 SO", "18.01.2025 SA", "08.02.2025 MI", "22.02.2025 SO", "15.03.2025 SA", "05.04.2025 SO", "26.04.2025 SO", "10.05.2025 SA", "24.05.2025 SA", "14.06.2025 MI", "28.06.2025 SA", "12.07.2025 SO", "26.07.2025 SO", "09.08.2025 SA", "16.08.2025 SA", "17.08.2025 SA"],
             },
             {
                 title: "Vinschgau K23",
@@ -79,7 +160,6 @@ const pageData = ref<any>({
                     text: "Kräuterkurs Anmelden",
                     href: "/kurse/pustertal-k20",
                 },
-                dates: ["28.10.2023 SA", "11.11.2023 SA", "09.12.2023 SO", "20.01.2024 SA", "10.02.2024 SO", "23.03.2024 SA", "27.04.2024 SA", "11.05.2024 SA", "08.06.2024 SA", "06.07.2024 SO", "03.08.2024 SO", "31.08.2024 MI", "14.09.2024 SA", "28.09.2024 SO", "12.10.2024 SA", "09.11.2024 SA", "23.11.2024 SA", "07.12.2024 SO", "18.01.2025 SA", "08.02.2025 MI", "22.02.2025 SO", "15.03.2025 SA", "05.04.2025 SO", "26.04.2025 SO", "10.05.2025 SA", "24.05.2025 SA", "14.06.2025 MI", "28.06.2025 SA", "12.07.2025 SO", "26.07.2025 SO", "09.08.2025 SA", "16.08.2025 SA", "17.08.2025 SA"],
             },
             {
                 title: "Unterland K21",
@@ -91,7 +171,6 @@ const pageData = ref<any>({
                     text: "Kräuterkurs Anmelden",
                     href: "/kurse/pustertal-k20",
                 },
-                dates: ["28.10.2023 SA", "11.11.2023 SA", "09.12.2023 SO", "20.01.2024 SA", "10.02.2024 SO", "23.03.2024 SA", "27.04.2024 SA", "11.05.2024 SA", "08.06.2024 SA", "06.07.2024 SO", "03.08.2024 SO", "31.08.2024 MI", "14.09.2024 SA", "28.09.2024 SO", "12.10.2024 SA", "09.11.2024 SA", "23.11.2024 SA", "07.12.2024 SO", "18.01.2025 SA", "08.02.2025 MI", "22.02.2025 SO", "15.03.2025 SA", "05.04.2025 SO", "26.04.2025 SO", "10.05.2025 SA", "24.05.2025 SA", "14.06.2025 MI", "28.06.2025 SA", "12.07.2025 SO", "26.07.2025 SO", "09.08.2025 SA", "16.08.2025 SA", "17.08.2025 SA"],
             },
         ],
     },
