10eb5a2d1a7f72242444b42d1749bd09cbf2dd1f
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-12 14:36:46 +0000

    data: add content for page kontakt

--- app/pages/kontakt.vue
+++ app/pages/kontakt.vue
@@ -1,5 +1,36 @@
-<template></template>
+<template>
+    <HeroBig :title="pageData.herobig.title" />
+    <ImageText :image="pageData.contact.image" :text1="pageData.contact.text1" />
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const pageData = ref<any>({
+    herobig: {
+        title: "Sigrid Thaler Rizzolli",
+    },
+    contact: {
+        image: {
+            src: "/images/sigrid/sigrid_02.jpg",
+            alt: "Sigrid Thaler Rizzolli",
+        },
+        text1: String.raw`
+            <address>
+                <h3>Handy</h3>
+                <p>
+                    <a href="tel:+39 3382 698 477" class="link">+39 3382 698 477</a>
+                </p>
+                <h3>E-Mail</h3>
+                <p>
+                    <a href="mailto:sigrid.thaler@gmail.com" class="link">sigrid.thaler@gmail.com</a>
+                </p>
+                <h3>Wohnort</h3>
+                <p>
+                    IT 39040 Montan, Weingütl Nr. 6
+                </p>
+            </address>
+        `,
+    },
+});
+</script>
 
 <style lang="scss" scoped></style>
