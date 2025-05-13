e18035349b7c0d7e57728243ea3e13c9e80d1767
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-13 06:12:09 +0000

    style(CourseList): optimize CourseList component for mobile

--- app/components/sections/CourseList.vue
+++ app/components/sections/CourseList.vue
@@ -46,4 +46,42 @@ const props = defineProps<CourseListProps>();
     @include col-start(8);
     @include col(4);
 }
+
+@media (max-width: 1023px) {
+    .contentelement_courselist {
+        margin-bottom: 7.5rem;
+    }
+
+    .title {
+        @include col-start(3);
+        @include col(11);
+
+        & {
+            margin-bottom: 4.5rem;
+        }
+    }
+
+    .course:nth-child(3n + 2) {
+        @include col-start(2);
+        @include col(10);
+
+        & {
+            margin-top: 0;
+        }
+    }
+
+    .course:nth-child(3n) {
+        @include col-start(3);
+        @include col(10);
+    }
+
+    .course:nth-child(3n + 1) {
+        @include col-start(2);
+        @include col(10);
+    }
+
+    .course:not(:last-of-type) {
+        margin-bottom: 6rem;
+    }
+}
 </style>
