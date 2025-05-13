5c872e7453fb76b3cc6c01120a2478344c5458a6
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-13 13:29:41 +0000

    feat(Footer): move footer links to separate FooterLinks component

--- app/components/layout/Footer/Footer.vue
+++ app/components/layout/Footer/Footer.vue
@@ -15,13 +15,7 @@
         </div>
         <div class="text2">Kräuterakademie Südtirol</div>
         <FooterSocials />
-        <nav class="footer-links">
-            <NuxtLink class="link" to="/impressum">Impressum</NuxtLink>
-            <span class="sep">.</span>
-            <NuxtLink class="link" to="/privacy">Privacy</NuxtLink>
-            <span class="sep">.</span>
-            <NuxtLink class="link" to="/cookies">Cookies</NuxtLink>
-        </nav>
+        <FooterLinks />
         <FooterPartner />
     </footer>
 </template>
@@ -95,33 +89,6 @@ footer {
     }
 }
 
-.footer-links {
-    @include col-start(11);
-    @include col(3);
-
-    & {
-        height: 100%;
-
-        display: flex;
-        justify-content: end;
-        align-items: center;
-    }
-
-    .sep {
-        padding: 0 0.5rem;
-    }
-
-    a {
-        color: $colorFont;
-        text-decoration: none;
-
-        &:hover {
-            color: $colorCta;
-            text-decoration: underline;
-        }
-    }
-}
-
 @media (max-width: 1023px) {
     footer {
         row-gap: 0;
@@ -178,16 +145,6 @@ footer {
 
         & {
             padding-bottom: 4.5rem;
-        }
-    }
-
-    .footer-links {
-        @include col-start(2);
-        @include col(12);
-
-        & {
-            padding-bottom: 1.5rem;
-            justify-content: left;
         }
     }
 }
---
+++ app/components/layout/Footer/FooterLinks.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav class="footer-links">
+        <NuxtLink class="link" to="/impressum">Impressum</NuxtLink>
+        <span class="sep">.</span>
+        <NuxtLink class="link" to="/privacy">Privacy</NuxtLink>
+        <span class="sep">.</span>
+        <NuxtLink class="link" to="/cookies">Cookies</NuxtLink>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+import { gsap } from "gsap";
+
+onMounted(() => {});
+</script>
+
+<style lang="scss" scoped>
+.footer-links {
+    @include col-start(11);
+    @include col(3);
+
+    & {
+        height: 100%;
+
+        display: flex;
+        justify-content: end;
+        align-items: center;
+    }
+
+    .sep {
+        padding: 0 0.5rem;
+    }
+
+    a {
+        color: $colorFont;
+        text-decoration: none;
+
+        &:hover {
+            color: $colorCta;
+            text-decoration: underline;
+        }
+    }
+}
+
+@media (max-width: 1023px) {
+    .footer-links {
+        @include col-start(2);
+        @include col(12);
+
+        & {
+            padding-bottom: 1.5rem;
+            justify-content: left;
+        }
+    }
+}
+</style>
