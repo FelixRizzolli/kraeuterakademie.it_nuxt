222f4da5d26d910a82a56f7a6b08df24fce2bd20
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-13 08:03:12 +0000

    data: update content for homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,6 @@
 <template>
     <HeroSmall :title="pageData.herosmall.title" :image="pageData.herosmall.image" :link="pageData.herosmall.link" />
-    <ImageText :image="pageData.imagetext.image" :text1="pageData.imagetext.text1" :infos="pageData.imagetext.infos" :text2="pageData.imagetext.text2" :link="pageData.imagetext.link" />
     <AnimatedText :text="pageData.animatedtext.text" />
-    <MoodPicture :image="pageData.moodpicture.image" />
-    <Infos :infos="pageData.infos.infos" />
 </template>
 
 <script lang="ts" setup>
@@ -11,51 +8,16 @@ const pageData = ref<any>({
     herosmall: {
         title: "Kurse zu Wildpflanzen und Heilkräuter",
         image: {
-            src: "/images/herosmall_1.png",
+            src: "/images/herosmall_2.png",
             alt: "Kräuterakademie",
         },
         link: {
-            href: "/akademie",
+            href: "/kraeuterkurse",
             text: "Kräuterakademie",
         },
     },
-    imagetext: {
-        image: {
-            src: "/images/imgtext_1.png",
-            alt: "Seminare",
-        },
-        text1: "Ich bin ein kleiner Blindtext. Und zwar schon so lange ich denken kann. Es war nicht leicht zu verstehen, was es bedeutet, ein blinder Text zu sein: Man ergibt keinen Sinn. Wirklich keinen Sinn. Man wird zusammenhangslos eingeschoben und rumgedreht - und oftmals gar nicht erst gelesen. Aber bin ich allein deshalb ein Text.",
-        infos: "Finde zu deiner Natur und entfalte deine Kraft.",
-        text2: "Ich bin ein kleiner Blindtext. Und zwar schon so lange ich denken kann. Es war nicht leicht zu verstehen, was es bedeutet, ein blinder Text zu sein: Man ergibt keinen Sinn. Wirklich keinen Sinn. Man wird zusammenhangslos eingeschoben",
-        link: {
-            href: "/seminare",
-            text: "Link Seminare",
-        },
-    },
     animatedtext: {
-        text: "Ich bin ein kleiner Blindtext. Und zwar schon so lange ich denken kann. Es war nicht leicht zu verstehen, was es bedeutet, ein blinder Text zu sein: Man ergibt keinen Sinn. Wirklich keinen Sinn. Man wird zusammenhangslos eingeschoben und rumgedreht - und oftmals gar nicht erst gelesen. Aber bin ich allein deshalb ein Text.",
-    },
-    moodpicture: {
-        image: {
-            src: "/images/herobig_1.png",
-            alt: "Kräuterakademie",
-        },
-    },
-    infos: {
-        infos: [
-            {
-                title: "Lehrgangskosten € 270 + Mwst",
-                text: "Im Preis inbegriffen sind umfangreiche Unterlagen und Bestimmungshilfen",
-            },
-            {
-                title: "Teilnehmerzahl ist begrenzt",
-                text: "Bei Fragen oder Infos: 338 269 8477 oder sigrid.thaler@gmail.com",
-            },
-            {
-                title: "Referentin Sigrid Thaler",
-                text: "Referentin der FNL Kräuterakademie",
-            },
-        ],
+        text: "Alles was wir brauchen, um gesund zu bleiben, hat uns die Natur reichlich geschenkt. ~ Sebastian Kneipp",
     },
 });
 </script>
