a888370a2f2b9950a915e1a56af39bac4bdbdcf1
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-13 16:25:42 +0000

    data: fix typo

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,6 +7,9 @@
 </template>
 
 <script lang="ts" setup>
+import type { FooterProps } from '~/components/layout/Footer/Footer.vue';
+import type { HeaderProps } from '~/components/layout/Header/Header.vue';
+
 interface PageData {
     header: HeaderProps;
     footer: FooterProps;
