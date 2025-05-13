c86aede558a6a715e39f2c6df85fc4bf119b0cdf
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-13 13:11:57 +0000

    feat(Footer): refactor partner section into separate component

--- app/components/layout/Footer/Footer.vue
+++ app/components/layout/Footer/Footer.vue
@@ -31,17 +31,7 @@
             <span class="sep">.</span>
             <NuxtLink class="link" to="/cookies">Cookies</NuxtLink>
         </nav>
-        <div class="partner">
-            <NuxtLink to="https://fnl-kraeuterakademie.at/">
-                <img src="/images/partner/fnl_akademie_logo.png" alt="FML Kräuterakademie nach Ignaz Schlifni" />
-            </NuxtLink>
-            <NuxtLink to="https://fnl.at/">
-                <img src="/images/partner/fnl_experte_logo.png" alt="FML Kräuterexperte*in nach Ignaz Schlifni" />
-            </NuxtLink>
-            <NuxtLink to="http://www.suedtiroler-kraeuterpaedagogen.it/">
-                <img src="/images/partner/skp_logo.png" alt="Südtiroler Kräuterpädagogen" />
-            </NuxtLink>
-        </div>
+        <FooterPartner />
     </footer>
 </template>
 
@@ -173,24 +163,7 @@ footer {
     }
 }
 
-.partner {
-    @include col-start(1);
-    @include col(14);
-
-    padding: 5.5rem;
-    background-color: $colorBackgroundCream;
-
-    display: flex;
-    justify-content: end;
-    gap: 4.5rem;
-
-    img {
-        height: 9rem;
-        width: auto;
-    }
-}
-
-@media (max-width: 1024px) {
+@media (max-width: 1023px) {
     footer {
         row-gap: 0;
     }
@@ -279,17 +252,6 @@ footer {
         & {
             padding-bottom: 1.5rem;
             justify-content: left;
-        }
-    }
-
-    .partner {
-        padding: 2.5rem;
-
-        justify-content: start;
-        gap: 2rem;
-
-        img {
-            height: 4.4rem;
         }
     }
 }
---
+++ app/components/layout/Footer/FooterPartner.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="partner">
+        <NuxtLink to="https://fnl-kraeuterakademie.at/">
+            <img src="/images/partner/fnl_akademie_logo.png" alt="FML Kräuterakademie nach Ignaz Schlifni" />
+        </NuxtLink>
+        <NuxtLink to="https://fnl.at/">
+            <img src="/images/partner/fnl_experte_logo.png" alt="FML Kräuterexperte*in nach Ignaz Schlifni" />
+        </NuxtLink>
+        <NuxtLink to="http://www.suedtiroler-kraeuterpaedagogen.it/">
+            <img src="/images/partner/skp_logo.png" alt="Südtiroler Kräuterpädagogen" />
+        </NuxtLink>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.partner {
+    @include col-start(1);
+    @include col(14);
+
+    padding: 5.5rem;
+    background-color: $colorBackgroundCream;
+
+    display: flex;
+    justify-content: end;
+    gap: 4.5rem;
+
+    img {
+        height: 9rem;
+        width: auto;
+    }
+}
+
+@media (max-width: 1023px) {
+    .partner {
+        padding: 2.5rem;
+
+        justify-content: start;
+        gap: 2rem;
+
+        img {
+            height: 4.4rem;
+        }
+    }
+}
+</style>
