bc1831248b5db108c546646e03be86f6da446397
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-15 18:58:53 +0000

    feat(storybook): add HeroSmall story

---
+++ app/components/sections/HeroSmall.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import HeroSmall from "./HeroSmall.vue";
+
+const meta: Meta<typeof HeroSmall> = {
+    component: HeroSmall,
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroSmall>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+    render: (args) => ({
+        components: { HeroSmall },
+        setup() {
+            return { args };
+        },
+        template: '<HeroSmall v-bind="args" />',
+    }),
+    args: {
+        data: {
+            title: "Kurse zu Wildpflanzen und Heilkräuter",
+            image: {
+                src: "/images/herosmall_2.png",
+                alt: "Kräuterakademie",
+            },
+            link: {
+                href: "/kraeuterkurse",
+                text: "Kräuterakademie",
+            },
+        },
+    },
+};
