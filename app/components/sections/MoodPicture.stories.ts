e36b98e010aa54e43a4054a9052dbdfc7e94df95
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-15 19:18:48 +0000

    feat(storybook): add MoodPicture story

---
+++ app/components/sections/MoodPicture.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import MoodPicture from "./MoodPicture.vue";
+
+const meta: Meta<typeof MoodPicture> = {
+    component: MoodPicture,
+};
+
+export default meta;
+type Story = StoryObj<typeof MoodPicture>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+    render: (args) => ({
+        components: { MoodPicture },
+        setup() {
+            return { args };
+        },
+        template: '<MoodPicture v-bind="args" />',
+    }),
+    args: {
+        data: {
+            image: {
+                src: "/images/herobig_2.png",
+                alt: "Kräuterkurs Bild",
+            },
+        },
+    },
+};
