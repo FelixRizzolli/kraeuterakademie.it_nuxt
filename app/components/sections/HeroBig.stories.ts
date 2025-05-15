7334053d65a0f0378e28af8617478e9c587f62ff
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-05-15 18:58:36 +0000

    feat(storybook): add HeroBig story

---
+++ app/components/sections/HeroBig.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import HeroBig from "./HeroBig.vue";
+
+const meta: Meta<typeof HeroBig> = {
+    component: HeroBig,
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroBig>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+    render: (args) => ({
+        components: { HeroBig },
+        setup() {
+            return { args };
+        },
+        template: '<HeroBig v-bind="args" />',
+    }),
+    args: {
+        data: {
+            title: "Aufbaulehrgang Knospenexpert*in",
+            image: {
+                src: "/images/herobig_1.png",
+                alt: "Kräuterkurs Bild",
+            },
+        },
+    },
+};
