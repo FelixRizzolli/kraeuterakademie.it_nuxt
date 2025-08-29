44fc5bf2bf822b52856b39ec79afc6f4a82a8a0b
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-08-29 16:05:25 +0000

    chore(dependencies): upgrade @storybook/* packages from 8.6.14 to 9.1.3

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook-vue/nuxt';
 
 const config: StorybookConfig = {
     stories: ['../app/components/**/*.mdx', '../app/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-    addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/experimental-addon-test'],
+    addons: [
+        '@chromatic-com/storybook',
+        '@storybook/addon-vitest',
+        '@storybook/addon-docs'
+    ],
     framework: {
         name: '@storybook-vue/nuxt',
         options: {},
