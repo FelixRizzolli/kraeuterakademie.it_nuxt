d2d3a9c0fe02b1e11e7eed7928edf95f97bc9c99
Felix Rizzolli <felix.rizzolli@outlook.de>
2026-01-01 19:01:15 +0000

    feat(dashboard): add otp field to signup template

---
+++ layers/dashboard/components/ui/field/FieldInputOTP.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import {
+        InputOTP,
+        InputOTPGroup,
+        InputOTPSeparator,
+        InputOTPSlot,
+    } from '~~/layers/dashboard/components/ui/input-otp';
+</script>
+
+<template>
+    <InputOTP :maxlength="8">
+        <InputOTPGroup>
+            <InputOTPSlot :index="0" />
+            <InputOTPSlot :index="1" />
+            <InputOTPSlot :index="2" />
+            <InputOTPSlot :index="3" />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+            <InputOTPSlot :index="4" />
+            <InputOTPSlot :index="5" />
+            <InputOTPSlot :index="6" />
+            <InputOTPSlot :index="7" />
+        </InputOTPGroup>
+    </InputOTP>
+</template>
--- layers/dashboard/pages/signup.vue
+++ layers/dashboard/pages/signup.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
-import SignupForm from '~~/layers/dashboard/components/SignupForm.vue'
-</script>
-
 <template>
-  <div class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-    <div class="w-full max-w-sm md:max-w-4xl">
-      <SignupForm />
+    <div class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+        <div class="w-full max-w-sm md:max-w-4xl">
+            <SignupForm />
+        </div>
     </div>
-  </div>
 </template>
+
+<script setup lang="ts">
+    import SignupForm from '~~/layers/dashboard/components/SignupForm.vue';
+
+    definePageMeta({
+        layout: 'dashboard',
+    });
+</script>
