acfce9b587be26c86d6abf780ef842df57df4a25
Felix Rizzolli <felix.rizzolli@outlook.de>
2026-01-02 07:18:23 +0000

    feat(dashboard): update sidebar links

--- layers/dashboard/components/AppSidebar.vue
+++ layers/dashboard/components/AppSidebar.vue
@@ -1,188 +1,175 @@
-<script setup lang="ts">
-import type { SidebarProps } from '~~/layers/dashboard/components/ui/sidebar'
-
-import {
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  LifeBuoy,
-  Map,
-  PieChart,
-  Send,
-  Settings2,
-  SquareTerminal,
-} from "lucide-vue-next"
-
-import NavMain from '~~/layers/dashboard/components/NavMain.vue'
-import NavProjects from '~~/layers/dashboard/components/NavProjects.vue'
-import NavSecondary from '~~/layers/dashboard/components/NavSecondary.vue'
-import NavUser from '~~/layers/dashboard/components/NavUser.vue'
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from '~~/layers/dashboard/components/ui/sidebar'
-
-const props = withDefaults(defineProps<SidebarProps>(), {
-  variant: "inset",
-})
-
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Support",
-      url: "#",
-      icon: LifeBuoy,
-    },
-    {
-      title: "Feedback",
-      url: "#",
-      icon: Send,
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
-</script>
-
 <template>
-  <Sidebar v-bind="props">
-    <SidebarHeader>
-      <SidebarMenu>
-        <SidebarMenuItem>
-          <SidebarMenuButton size="lg" as-child>
-            <a href="#">
-              <div class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <Command class="size-4" />
-              </div>
-              <div class="grid flex-1 text-left text-sm leading-tight">
-                <span class="truncate font-medium">Acme Inc</span>
-                <span class="truncate text-xs">Enterprise</span>
-              </div>
-            </a>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      </SidebarMenu>
-    </SidebarHeader>
-    <SidebarContent>
-      <NavMain :items="data.navMain" />
-      <NavProjects :projects="data.projects" />
-      <NavSecondary :items="data.navSecondary" class="mt-auto" />
-    </SidebarContent>
-    <SidebarFooter>
-      <NavUser :user="data.user" />
-    </SidebarFooter>
-  </Sidebar>
+    <Sidebar v-bind="props">
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" as-child>
+                        <a href="#">
+                            <div
+                                class="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground"
+                            >
+                                <i-logo-small class="size-7" />
+                            </div>
+                            <div class="grid flex-1 text-left text-sm leading-tight">
+                                <span class="truncate font-medium">{{ $t('dashboard.navigation.title') }}</span>
+                                <span class="truncate text-xs">{{ $t('dashboard.navigation.subtitle') }}</span>
+                            </div>
+                        </a>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+            <NavMain :items="data.navCourse" />
+            <NavMain :items="data.navStudy" />
+            <NavSecondary :items="data.navSecondary" class="mt-auto" />
+        </SidebarContent>
+        <SidebarFooter>
+            <NavUser :user="data.user" />
+        </SidebarFooter>
+    </Sidebar>
 </template>
+
+<script setup lang="ts">
+    import type { SidebarProps } from '~~/layers/dashboard/components/ui/sidebar';
+
+    import {
+        BookOpenText,
+        CalendarDays,
+        FileClock,
+        GraduationCap,
+        LifeBuoy,
+        NotebookPen,
+        Send,
+        Video,
+    } from 'lucide-vue-next';
+
+    import NavMain from '~~/layers/dashboard/components/NavMain.vue';
+    import NavSecondary from '~~/layers/dashboard/components/NavSecondary.vue';
+    import NavUser from '~~/layers/dashboard/components/NavUser.vue';
+    import {
+        Sidebar,
+        SidebarContent,
+        SidebarFooter,
+        SidebarHeader,
+        SidebarMenu,
+        SidebarMenuButton,
+        SidebarMenuItem,
+    } from '~~/layers/dashboard/components/ui/sidebar';
+
+    const props = withDefaults(defineProps<SidebarProps>(), {
+        variant: 'inset',
+    });
+
+    const data = {
+        user: {
+            name: 'Sigrid Thaler Rizzolli',
+            email: 'sigrid.thaler@gmail.com',
+            avatar: '/avatars/shadcn.jpg',
+        },
+        navCourse: [
+            {
+                title: $t('dashboard.navigation.nav-course.courses.title'),
+                url: '#',
+                icon: GraduationCap,
+                isActive: true,
+                items: [
+                    {
+                        title: $t('dashboard.navigation.nav-course.courses.items.overview'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-course.courses.items.documents'),
+                        url: '#',
+                    },
+                ],
+            },
+            {
+                title: $t('dashboard.navigation.nav-course.course-modules.title'),
+                url: '#',
+                icon: CalendarDays,
+                items: [
+                    {
+                        title: $t('dashboard.navigation.nav-course.course-modules.items.overview'),
+                        url: '#',
+                    },
+                ],
+            },
+        ],
+        navStudy: [
+            {
+                title: $t('dashboard.navigation.nav-study.video-lessons'),
+                url: '#',
+                icon: Video,
+                isActive: true,
+            },
+            {
+                title: $t('dashboard.navigation.nav-study.plant-lexicon.title'),
+                url: '#',
+                icon: BookOpenText,
+                items: [
+                    {
+                        title: $t('dashboard.navigation.nav-study.plant-lexicon.items.plants'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.plant-lexicon.items.poison-plants'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.plant-lexicon.items.plant-families'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.plant-lexicon.items.plant-groups'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.plant-lexicon.items.catalogue'),
+                        url: '#',
+                    },
+                ],
+            },
+            {
+                title: $t('dashboard.navigation.nav-study.quiz.title'),
+                url: '#',
+                icon: NotebookPen,
+                items: [
+                    {
+                        title: $t('dashboard.navigation.nav-study.quiz.items.plants-practice'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.quiz.items.plants-theory'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.quiz.items.plants-poison'),
+                        url: '#',
+                    },
+                    {
+                        title: $t('dashboard.navigation.nav-study.quiz.items.catalogue'),
+                        url: '#',
+                    },
+                ],
+            },
+        ],
+        navSecondary: [
+            {
+                title: $t('dashboard.navigation.nav-secondary.support'),
+                url: '#',
+                icon: LifeBuoy,
+            },
+            {
+                title: $t('dashboard.navigation.nav-secondary.feedback'),
+                url: '#',
+                icon: Send,
+            },
+            {
+                title: $t('dashboard.navigation.nav-secondary.changelog'),
+                url: '#',
+                icon: FileClock,
+            },
+        ],
+    };
+</script>
