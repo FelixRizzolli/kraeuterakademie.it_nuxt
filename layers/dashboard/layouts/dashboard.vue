6166e529dc624794ccd228c9515ddadd000c2621
Felix Rizzolli <felix.rizzolli@outlook.de>
2026-01-03 07:41:32 +0000

    feat(dashboard): move sidebar to layout

--- layers/dashboard/layouts/dashboard.vue
+++ layers/dashboard/layouts/dashboard.vue
@@ -1,10 +1,35 @@
 <template>
     <div class="min-h-screen bg-background">
-        <slot />
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header class="flex h-16 shrink-0 items-center gap-2">
+                    <div class="flex items-center gap-2 px-4">
+                        <SidebarTrigger class="-ml-1" />
+                        <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem class="hidden md:block">
+                                    <BreadcrumbLink href="#"> Building Your Application </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator class="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+                <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+                    <slot />
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     </div>
 </template>
 
 <script lang="ts" setup>
+    definePageMeta({ middleware: ['auth'] });
     import { useDashboardGlobalStore } from '~~/layers/dashboard/stores/dashboardGlobalStore';
 
     const dashboardGlobalStore = useDashboardGlobalStore();
